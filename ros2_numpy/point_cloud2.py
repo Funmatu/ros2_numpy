29150191e4aee453c16d49efef3a722979f7c2ad
Funmatu <funmatu@gmail.com>
2023-09-06 15:16:20 +0900

    Update point_cloud2.py

--- ros2_numpy/point_cloud2.py
+++ ros2_numpy/point_cloud2.py
@@ -144,3 +144,23 @@ def array_to_point_cloud2(np_array, frame_id=None):
     # msg.data = as_array
 
     return msg
+
+
+def get_xyz_points(cloud_array, remove_nans=True, dtype=float):
+    '''Pulls out x, y, and z columns from the cloud recordarray, and returns
+    a 3xN matrix.
+    '''
+    # remove crap points
+    if remove_nans:
+        mask = np.isfinite(cloud_array['x']) & \
+               np.isfinite(cloud_array['y']) & \
+               np.isfinite(cloud_array['z'])
+        cloud_array = cloud_array[mask]
+
+    # pull out x, y, and z values
+    points = np.zeros(cloud_array.shape + (3,), dtype=dtype)
+    points[...,0] = cloud_array['x']
+    points[...,1] = cloud_array['y']
+    points[...,2] = cloud_array['z']
+
+    return points
